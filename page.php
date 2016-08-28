f5e5ae2846bb4d0a86304431ca01c97641d21c92
TheJasonJones <jkjmr6@gmail.com>
2016-08-28 16:43:44 -0500

    General page template and main image

--- page.php
+++ page.php
@@ -1,7 +1,13 @@
-<?php get_header(); ?>
-  <div class="container">
-    <h1><?php the_title() ?></h1>
-
+<?php get_header(); the_post(); $featured = jj_get_featured_image( get_the_ID(), 'top_header_image' ); $f = get_fields(); ?>
+  <div class="bg-wrapper" style="background-image: url(<?php echo ( ( !empty( $featured ) ) ? $featured : '' ); ?>);">
+    <div class="container text">
+      <h1><?php the_title() ?></h1>
+      <div class="divider"></div>
+      <div><?php echo $f['quote'] ?><em><?php echo $f['source'] ?></em></div>
+    </div>
+  <div class="overlay"></div>
+</div>
+  <div class="container content">
     <div class="entry">
       <?php the_content(); ?>
     </div>
