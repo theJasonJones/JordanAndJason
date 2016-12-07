3c712c5918d8fd4e44f544ec199ae6da5f949a8f
Jason Jones <jkjmr6@gmail.com>
2016-12-07 10:40:24 -0600

    Update class.

--- instagram.php
+++ instagram.php
@@ -1,5 +1,40 @@
 <?php
+namespace Bolandish;
+
 class Instagram {
+
+    protected static function getContentsFromUrl($parameters) {
+        if (!function_exists('curl_init')) {
+            return false;
+        }
+        $random = self::generateRandomString();
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.instagram.com/query/");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'q='.$parameters);
+        $headers = array();
+        $headers[] = "Cookie:  csrftoken=$random;";
+        $headers[] = "X-Csrftoken: $random";
+        $headers[] = "Referer: https://www.instagram.com/";
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+
+
+        return $output;
+    }
+    protected static function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
     public static function getMediaByHashtag($hashtag = null, $count = 16, $assoc = false, $comment_count = false)
     {
         if ( empty($hashtag) || !is_string($hashtag) )
@@ -13,8 +48,7 @@ class Instagram {
         }
         $hashtag = strtolower($hashtag);
         $parameters = urlencode("ig_hashtag($hashtag) { media.first($count) {   count,   nodes {     caption,     code,   $comments,     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
-        $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
-        $media = json_decode(file_get_contents($url), ($assoc || $assoc == "array"));
+        $media = json_decode(static::getContentsFromUrl($parameters), ($assoc || $assoc == "array"));
         if($assoc == "array")
             $media = $media["media"]["nodes"];
         else
@@ -34,8 +68,7 @@ class Instagram {
             $comments = "comments {       count     }";
         }
         $parameters = urlencode("ig_user($user) { media.first($count) {   count,   nodes {     caption,     code,     $comments,     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
-        $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
-        $media = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        $media = json_decode(static::getContentsFromUrl($parameters),($assoc || $assoc == "array"));
         if($assoc == "array")
             $media = $media["media"]["nodes"];
         else
@@ -57,8 +90,7 @@ class Instagram {
 
         $parameters = urlencode("ig_user($user) { media.after($min_id,$count) {   count,   nodes {     caption,     code,    $comments,   date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
 
-        $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
-        $media = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        $media = json_decode(static::getContentsFromUrl($parameters),($assoc || $assoc == "array"));
         if($assoc == "array")
             $media = $media["media"]["nodes"];
         else
@@ -73,8 +105,7 @@ class Instagram {
         $comments = "comments.last($count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
 
         $parameters = urlencode("ig_shortcode({$media_shortcode}) { $comments }");
-        $url = "https://www.instagram.com/query/?q=$parameters&ref=media%3A%3Ashow";
-        $comments = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        $comments = json_decode(static::getContentsFromUrl($parameters),($assoc || $assoc == "array"));
         if($assoc == "array")
             $comments = $comments["comments"]["nodes"];
         else
@@ -88,12 +119,11 @@ class Instagram {
         $comments = "comments.before($max_id, $count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
 
         $parameters = urlencode("ig_shortcode({$media_shortcode}) { $comments }");
-        $url = "https://www.instagram.com/query/?q=$parameters&ref=media%3A%3Ashow";
-        $comments = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        $comments = json_decode(static::getContentsFromUrl($parameters),($assoc || $assoc == "array"));
         if($assoc == "array")
             $comments = $comments["comments"]["nodes"];
         else
             $comments = $comments->comments->nodes;
         return $comments;
-    }    
+    }
 }
